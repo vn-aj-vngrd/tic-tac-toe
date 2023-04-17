66b8439bcb8776813d23f6c5e019a108f4bd4410
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2023-04-17 21:35:01 +0800

    fixes

--- src/test/TicTacToeTest.java
+++ src/test/TicTacToeTest.java
@@ -1,16 +1,80 @@
 package test;
 
 import main.lib.*;
+import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TicTacToeTest {
+    private Game game;
+    private Checker checker;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+        checker = new Checker();
+    }
 
     @Test
     public void testTicTacToeInitialization() {
-        Game game = new Game();
-        Checker checker = new Checker();
+        game = new Game();
+        checker = new Checker();
         assertNotNull(game);
         assertNotNull(checker);
+    }
+
+    @Test
+    public void testFullGameSimulation() {
+        game = new Game();
+        checker = new Checker();
+
+        // Simulate a game where player X wins
+        game.placeMark(0, 0);
+        game.changePlayer();
+
+        game.placeMark(1, 0);
+        game.changePlayer();
+
+        game.placeMark(0, 1);
+        game.changePlayer();
+
+        game.placeMark(1, 1);
+        game.changePlayer();
+
+        game.placeMark(0, 2);
+        game.changePlayer();
+
+        assertTrue(checker.checkForWin(game.getBoard()));
+        assertEquals('O', game.getCurrentPlayerMark());
+
+        // Simulate a game where it's a tie
+        game.placeMark(0, 0);
+        game.changePlayer();
+
+        game.placeMark(0, 1);
+        game.changePlayer();
+
+        game.placeMark(0, 2);
+        game.changePlayer();
+
+        game.placeMark(1, 1);
+        game.changePlayer();
+
+        game.placeMark(1, 0);
+        game.changePlayer();
+
+        game.placeMark(2, 0);
+        game.changePlayer();
+
+        game.placeMark(1, 2);
+        game.changePlayer();
+
+        game.placeMark(2, 1);
+        game.changePlayer();
+
+        game.placeMark(2, 2);
+        game.changePlayer();
+
+        assertTrue(checker.checkBoardFull(game.getBoard()));
     }
 }
